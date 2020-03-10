64d88e52e957ea8c41a8a484a65da6d9e4e51ed4
Nikolay Biryukov <n.biryukov.exceedteam@gmail.com>
2020-03-10 15:38:45 +0300

    modified aall files

--- Project/script.js
+++ Project/script.js
@@ -1,21 +1,77 @@
-let List = $('#tdlApp ul');
-let Mask = 'tdl_';
+$(document).ready(function () {
+    
+	$('.add-items').submit(function(event) 
+	{
+		event.preventDefault();
 
-function showTasks(){
-    // Узнаём размер хранилища
-    let Storage_size = localStorage.length;
-    // Если в хранилище что-то есть…
-    if (Storage_size > 0){
-      // то берём и добавляем это в задачи  
-      for (let i = 0; i < Storage_size; i++){
-        let key = localStorage.key(i);
-        if(key.indexOf(Mask) == 0){
-          // и делаем это элементами списка
-          $('<li></li>').addClass('tdItem')
-            .attr('data-itemid', key)
-            .text(localStorage.getItem(key))
-            .appendTo(List);
-        }
-      }
-    }
-}
+		let item = $('#todo-list-item').val();
+
+		if(item) 
+		{
+			$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><br></li>");
+			let num = Math.floor(Math.random()*6) + 1;
+			console.log(num);
+			let liColor = 0;
+			if (num==1) {
+				liColor = "#ee6a68";
+			} else if (num==2) {
+				liColor = "#f077a2";
+			} else if (num==3) {
+				liColor = "#9282bb";
+			} else if (num==4) {
+				liColor = "#00BFFF";
+			} else if (num==5) {
+				liColor = "#00CCCC";
+			} else if (num==6) {
+				liColor = "#FDDB6D";
+			}
+			$('li:last-child').css('background-color', liColor);
+			
+			$('#todo-list-item').val("");
+		}
+		
+	});
+
+	$(document).on('change', '.checkbox', function() 
+	{
+		//start event click six buttons
+		$('#color1').click(function(){
+			$('li.completed').attr('style', 'background-color: #ee6a68;');
+		});
+		$('#color2').click(function(){
+			$('li.completed').attr('style', 'background-color: #f077a2;');
+		});
+		$('#color3').click(function(){
+			$('li.completed').attr('style', 'background-color: #9282bb;');
+		});
+		$('#color4').click(function(){
+			$('li.completed').attr('style', 'background-color: #00BFFF;');
+		});
+		$('#color5').click(function(){
+			$('li.completed').attr('style', 'background-color: #00CCCC;');
+		});
+		$('#color6').click(function(){
+			$('li.completed').attr('style', 'background-color: #FDDB6D;');
+		});
+		//end event click six buttons
+		if($(this).attr('checked')) 
+		{
+			$(this).removeAttr('checked');
+		} 
+		else 
+		{
+			$(this).attr('checked', 'checked');
+		}
+
+		$(this).parent().toggleClass('completed');
+		$(this);
+		
+	});
+
+	$(document).on('click', '.remove', function() 
+	{
+		$(this).parent().remove();
+
+	});
+
+});
